97b62ede9f16dcc364bcbf9a567aaeaa410ebe05
Rafał Florczak <florczak.raf@gmail.com>
2022-08-04 21:01:44 +0000

    Reformat code with black

--- serve.py
+++ serve.py
@@ -6,7 +6,8 @@ from flask.json import jsonify
 app = Flask("WindowPositionAssistantProxy")
 proxy_storage = defaultdict(list)
 
-@app.route("/<int:proxy_id>", methods=['GET', 'POST'])
+
+@app.route("/<int:proxy_id>", methods=["GET", "POST"])
 def proxy(proxy_id):
     if request.method == "GET":
         return jsonify(proxy_storage[proxy_id])
